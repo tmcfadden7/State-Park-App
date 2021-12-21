abfd10716e0883eb30410b53155875504dc8a61d
Thomas McFadden <tmac727@yahoo.com>
2021-12-20 19:06:21 -0500

    Create StateParkGrid.js, add divs with bootstrap grid classes

---
+++ src/components/StateParksGrid.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const StateParksGrid = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default StateParksGrid
