37a03ddb1e874f229fbf38ab31bad417224e3c07
Thomas McFadden <tmac727@yahoo.com>
2021-12-20 21:19:43 -0500

    Add img to jumbotron and remove default css styles

--- src/components/JumboTron.js
+++ src/components/JumboTron.js
@@ -1,13 +1,17 @@
+import JumboImage from '../../src/images/Ohio-Hocking-Hills-state-park.jpeg'
 
 const JumboTron = () => {
     return (
         <>
-            <div class="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4" />
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            <div class="jumbotron py-0">
+                <div className="container">
+                    <img className="img-fluid" src={JumboImage} alt="" />
+                    <h1 class="display-4">Hello, world!</h1>
+                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <hr class="my-4" />
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </div>
             </div>
         </>
     )
