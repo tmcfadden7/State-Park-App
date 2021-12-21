1ceecf500ea09da616be1ffe1b03ef551eb08170
Thomas McFadden <tmac727@yahoo.com>
2021-12-20 21:52:50 -0500

    Add gradient background and change text to white in jumbotron

--- src/components/JumboTron.js
+++ src/components/JumboTron.js
@@ -3,14 +3,18 @@ import JumboImage from '../../src/images/Ohio-Hocking-Hills-state-park.jpeg'
 const JumboTron = () => {
     return (
         <>
-            <div class="jumbotron py-0">
+            <div class="jumbotron py-3">
                 <div className="container">
-                    <img className="img-fluid" src={JumboImage} alt="" />
-                    <h1 class="display-4">Hello, world!</h1>
-                    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <hr class="my-4" />
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                    <div className="row">
+                        <div className="col-12 col-lg-8">
+                            <img className="img-fluid" src={JumboImage} alt="" />
+                        </div>
+                        <div className="col-12 col-lg-4 text-white">
+                            <h1 class="display-4">Hello, world!</h1>
+                            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                            <a class="btn btn-secondary btn-lg" href="#" role="button">Learn more</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
