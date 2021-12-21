7308c6b5aadd77314f06589900df6953a5530d3e
Thomas McFadden <tmac727@yahoo.com>
2021-12-21 17:50:40 -0500

    Map through activities and display using bootstrap collapse

--- src/components/StateParks.js
+++ src/components/StateParks.js
@@ -5,15 +5,26 @@ const StateParks = ({ parks }) => {
         <>
             {parks.data.map((park) => {
         // console.log(park.activities[1].name)
-  
           return (
+            <>
             <div className="col-sm-4" key={park.id}>
               <h1>{park.fullName}</h1>
+              <p>{park.addresses[0].line1}, <br/>
+              {park.addresses[0].city}, {park.addresses[0].stateCode} {park.addresses[0].postalCode}</p>
               <p>{park.description}</p>
+              <button className="btn btn-secondary" type='button' data-toggle='collapse' data-target='#activities-collapse'>See Activities</button>
+              <div>
+                <ul className='list-group list-group-flush'>
+                  {park.activities.map((activity) => {
+                    return (
+                    <li key={activity.id} id='activities-collapse' className='list-group-item collapse'>{activity.name}</li>
+                    )
+                  })}
+                </ul>
+              </div>
            </div>
+           </>
           )
-        
-        
       })}
         </>
     )
